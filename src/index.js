379dc05911ff6fabdd9cddcb28dd935e25a4e195
maryamabueleid <118001974+maryamabueleid@users.noreply.github.com>
2023-09-25 09:48:44 +0200

    weather icon

--- src/index.js
+++ src/index.js
@@ -13,26 +13,30 @@ function displayTemperature(response) {
   let wind = document.querySelector("#wind");
   wind.innerHTML = Math.round(response.data.wind.speed);
   let time = document.querySelector("#date");
-  time.innerHTML = formatDate(response.data.time);
+  time.innerHTML = formatDate(response.data.time * 1000);
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  icon.setAttribute("alt", response.data.condition.description);
 }
 function formatDate(timestamp) {
   let date = new Date(timestamp);
-  let hours = (date.getHours() < 10 ? "0" : "") + date.getHours();
-  let minutes = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-  let days = [
-    "sunday",
-    "monday",
-    "tuesday",
-    "wednesday",
-    "thursday",
-    "friday",
-    "saturday",
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+  let days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+  let Day = days[date.getDay()];
+  return `${Day} ${hours}:${minutes}`;
 }
-
+let city = "cairo";
 let apiKey = "0bb6doe3a544328d20t3a076f6957409";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
